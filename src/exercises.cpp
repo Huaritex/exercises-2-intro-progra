28673d379192ed222707a98da089e28e39c7ee37
Huaritex <159574524+Huaritex@users.noreply.github.com>
2024-04-01 09:45:42 -0400

    si

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -5,7 +5,14 @@
 using namespace std;
 
 void exercise_1(string s1) {
-  // TODO: YOUR CODE HERE
+string_s1 ;
+getline(cin, string_s1)
+
+for(int i = 0; <= string_s1.size(); ++1){
+if(string_s1{i} == ' '){
+  cout << i << endl;
+}  
+cout << string_s1.size() << endl;
 }
 
 void exercise_2(string s1) {

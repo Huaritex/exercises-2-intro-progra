77b3b709013f13fb7ebb10bd55d51d6c030017c5
Huaritex <159574524+Huaritex@users.noreply.github.com>
2024-04-14 17:12:24 -0400

    fack

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -155,18 +155,18 @@ void exercise_15(int decimal) {
 }
 
 void exercise_16(int divident, int divider) {
-  int cociente = 0;
-  int residuo = divident;
-  int i = 0;
-  if(divider != 0){
-    for(i; residuo - divider >= 0; ++i;){
-      residuo = residuo - divider;
-    }
+ int cociente = 0;
+ int residuo = divident;
+ int i = 0;
+    if(divider!=0){
+        for(i; residuo-divider >= 0; ++i){
+            residuo = residuo - divider;
+            
+        }
     cociente = i;
     cout << cociente << " ";
     cout << residuo << endl;
-  }
-
+    }
 }
 
 

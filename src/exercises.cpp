657c94894148d1c1967d9978d972af01a1ed4c10
Huaritex <159574524+Huaritex@users.noreply.github.com>
2024-04-14 18:26:53 -0400

    nom nom nom  nom

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -32,7 +32,21 @@ void exercise_2(string wasap) {
   }
 }
 
-void exercise_3(string cake) {}
+void exercise_3(string cake) {
+  int i = 0;
+    while(i < 20){
+        if(cake[i]=='1'){
+            cout << "Om-nom-nom :P" << endl;
+            i = i + 1;
+        }else if(cake[i] == '0'){
+            cout << "No cake :(" << endl;
+            break;
+        }else{
+            i = i + 1;
+        }
+    }
+}
+
 
 
 void exercise_4(int number) {

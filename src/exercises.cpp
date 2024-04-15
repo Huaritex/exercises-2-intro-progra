4befce87cc245ec6e4c521d5292d141e6c76f991
Huaritex <159574524+Huaritex@users.noreply.github.com>
2024-04-14 21:06:46 -0400

    kskdkkdfek

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -224,7 +224,7 @@ void exercise_11() {
   cout << "U0 = 1" << endl;
   for(int i = 1; 10 >= i; ++i){
     double result = UI/( i + 1);
-    cout << "U" << i << " " << UI << endl;
+    cout << "U" << i << " = " << UI << endl;
     UI = result;
   }
 }
